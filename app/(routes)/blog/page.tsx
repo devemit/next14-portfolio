65a96a8015fd1f7484a66489f6426462ec8ef1f1
Mitko <mitkoiliev505@gmail.comm>
2024-01-15 22:12:25 +0100

    typos fix

---
+++ app/(routes)/blog/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Blog() {
+  return (
+    <div className='h-screen flex items-center justify-center text-white text-2xl font-bold'>
+      This page is under construction.
+    </div>
+  );
+}
