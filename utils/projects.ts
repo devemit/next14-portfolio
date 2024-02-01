0adc99c2c515909bee0a2e7365e1bdfe3ebecd3d
Mitko <mitkoiliev505@gmail.comm>
2024-02-01 19:14:46 +0100

    added:projects

--- utils/projects.ts
+++ utils/projects.ts
@@ -10,6 +10,47 @@ export default [
       'Beach Bar Reform is a React web application designed to enhance the dining experience in restaurants by providing a seamless QR-scanning menu for customers. The application aims to reduce physical contact between customers and menus, making it more hygienic and convenient, especially in a post-pandemic world.',
   },
   {
+    title: 'Next14 E-commerce',
+    imgUrl:
+      'https://github.com/devemit/cinemania-movies/assets/107273888/ee3ded3a-4506-48d7-9052-89ce06269465',
+    tech: ['Typescript', 'NextJS-14', 'Tailwind CSS'],
+    liveSite: 'https://ultrashopmk.vercel.app/',
+    seeCode: 'https://https://github.com/devemit/next14-ecommerce',
+    description:
+      'This project is a modern e-commerce website developed using Next14(SSR) with TypeScript and styled with Tailwind CSS. The website provides users with a seamless shopping experience, allowing them to browse through a variety of products, view detailed product information, and add items to their shopping cart.',
+  },
+  {
+    title: 'Notify | Notes App',
+    imgUrl:
+      'https://github.com/devemit/codeart-test-project/assets/107273888/7075015b-6d2f-4964-aaa5-8fce6eeecb2c',
+    tech: ['Typescript', 'NextJS-13', 'TailwindCSS'],
+    liveSite: 'https://notifypro.vercel.app/',
+    seeCode: 'https://github.com/devemit/todo-app',
+    description:
+      'A web-based note-taking application built with React and TypeScript. Users can create, edit, view, and delete notes. Notes are saved locally using localStorage for persistence.',
+  },
+  {
+    title: 'Password Generator',
+    imgUrl:
+      'https://github.com/devemit/Weather-app-JS/assets/107273888/447b98a4-2149-444d-8bcb-cd561a29bfe1',
+    tech: ['Typescript', 'NextJS-13', 'Tailwind CSS'],
+    liveSite: 'https://password-generator-pro.vercel.app/',
+    seeCode: 'https://github.com/devemit/password-generator',
+    description:
+      'Create a secure and customizable password generator with this TypeScript and React application. Generate strong passwords with various options like length, special characters, and numbers. Easily copy the generated passwords to your clipboard for enhanced security and convenience.',
+  },
+  {
+    title: 'Crypto DEFI',
+    imgUrl:
+      'https://user-images.githubusercontent.com/107273888/218598621-557e03c0-f16e-42bc-aafa-d67373c9943d.png',
+    tech: ['Typescript', 'React', 'Tailwind CSS'],
+    liveSite: 'https://cryptowebdefi.netlify.app/',
+    seeCode: 'https://github.com/devemit/crypto-react',
+    description:
+      'In my recent project, I delved into the fascinating realm of cryptocurrencies by designing and developing a comprehensive website aimed at providing users with valuable insights, news, and resources related to the world of digital assets. This project was driven by the goal of creating a platform where both beginners and experienced enthusiasts can learn, stay updated, and engage with the rapidly evolving cryptocurrency landscape. ',
+  },
+
+  {
     title: 'E-Commerce Website',
     imgUrl:
       'https://user-images.githubusercontent.com/107273888/226775835-86d65498-a4f5-4216-8379-c348160de964.png',
@@ -17,38 +58,7 @@ export default [
     liveSite: 'https://mitkos-shop-site.netlify.app',
     seeCode: 'https://github.com/devemit/E-commerce-Website',
     description:
-      'Ratio store is a  e-commerce platform build with React, a popular JavaScript library for building user interfaces. The goal of this project was to develop a modern and user-friendly online shopping experience that would allow customers to browse products, add items to their cart, and complete the checkout process seamlessly. ',
-  },
-  {
-    title: 'Travel Blog',
-    imgUrl:
-      'https://github.com/devemit/Weather-app-JS/assets/107273888/fd3dfece-0ef1-4c3d-87f0-3e8c98822fbe',
-    tech: ['Typescript', 'Nextjs13', 'CSS'],
-    liveSite: 'https://traveldotblog.vercel.app/',
-    seeCode: 'https://github.com/devemit/travel-blog',
-    description:
-      'A web-based Travel Blog built using React and TypeScript. This project allows users to share their travel experiences, including photos and stories, in an interactive and visually appealing format.',
-  },
-  {
-    title: 'Notify | Notes App',
-    imgUrl:
-      'https://github.com/devemit/codeart-test-project/assets/107273888/7075015b-6d2f-4964-aaa5-8fce6eeecb2c',
-    tech: ['Typescript', 'Nextjs13', 'TailwindCSS'],
-    liveSite: 'https://notifypro.vercel.app/',
-    seeCode: 'https://github.com/devemit/todo-app',
-    description:
-      'A web-based note-taking application built with React and TypeScript. Users can create, edit, view, and delete notes. Notes are saved locally using localStorage for persistence.',
-  },
-
-  {
-    title: 'Glass Maestros',
-    imgUrl:
-      'https://github.com/devemit/codeart-test-project/assets/107273888/9a3b30e2-983a-4257-96b0-239b11b58d22',
-    tech: ['Typescript', 'Nextjs13', 'TailwindCSS'],
-    liveSite: 'https://glassmaestros.vercel.app/',
-    seeCode: 'https://github.com/devemit/business-promote-website',
-    description:
-      'Glass Maestros: A TypeScript and Tailwind CSS website that showcases the artistry of a glass installation firm, promoting their craftsmanship with a gallery of stunning installations and essential service information.The website not only demonstrates the companys proficiency in working with glass but also provides essential information about their services, contact details, and perhaps even client testimonials.',
+      'Ratio store is a  e-commerce platform build with React, a popular JavaScript library for building user interfaces. The goal of this project was to develop a modern and user-friendly online shopping experience that would allow customers to browse products, add items to their cart, and complete the checkout process seamlessly.',
   },
   {
     title: 'Cinemania/Netflix UI',
@@ -60,25 +70,45 @@ export default [
     description:
       'In my recent project, I embarked on an exciting journey to create a Netflix clone using React, a powerful JavaScript library for building user interfaces. The primary objective of this project was to recreate the iconic Netflix streaming experience, offering users a platform to explore a vast library of movies and TV shows and enjoy their favorite content effortlessly.',
   },
+  {
+    title: 'Travel Blog',
+    imgUrl:
+      'https://github.com/devemit/Weather-app-JS/assets/107273888/fd3dfece-0ef1-4c3d-87f0-3e8c98822fbe',
+    tech: ['Typescript', 'NextJS-13', 'CSS'],
+    liveSite: 'https://traveldotblog.vercel.app/',
+    seeCode: 'https://github.com/devemit/travel-blog',
+    description:
+      'A web-based Travel Blog built using React and TypeScript. This project allows users to share their travel experiences, including photos and stories, in an interactive and visually appealing format.',
+  },
 
   {
-    title: 'Crypto DEFI',
+    title: 'Glass Maestros',
     imgUrl:
-      'https://user-images.githubusercontent.com/107273888/218598621-557e03c0-f16e-42bc-aafa-d67373c9943d.png',
-    tech: ['Typescript', 'React', 'Tailwind CSS'],
-    liveSite: 'https://cryptowebdefi.netlify.app/',
-    seeCode: 'https://github.com/devemit/crypto-react',
+      'https://github.com/devemit/codeart-test-project/assets/107273888/9a3b30e2-983a-4257-96b0-239b11b58d22',
+    tech: ['Typescript', 'NextJS-13', 'TailwindCSS'],
+    liveSite: 'https://glassmaestros.vercel.app/',
+    seeCode: 'https://github.com/devemit/business-promote-website',
     description:
-      'In my recent project, I delved into the fascinating realm of cryptocurrencies by designing and developing a comprehensive website aimed at providing users with valuable insights, news, and resources related to the world of digital assets. This project was driven by the goal of creating a platform where both beginners and experienced enthusiasts can learn, stay updated, and engage with the rapidly evolving cryptocurrency landscape. ',
+      'Glass Maestros: A TypeScript and Tailwind CSS website that showcases the artistry of a glass installation firm, promoting their craftsmanship with a gallery of stunning installations and essential service information.The website not only demonstrates the companys proficiency in working with glass but also provides essential information about their services, contact details, and perhaps even client testimonials.',
   },
   {
-    title: 'Password Generator',
+    title: 'Astro Layout',
     imgUrl:
-      'https://github.com/devemit/Weather-app-JS/assets/107273888/447b98a4-2149-444d-8bcb-cd561a29bfe1',
-    tech: ['Typescript', 'Next13', 'Tailwind CSS'],
-    liveSite: 'https://password-generator-pro.vercel.app/',
-    seeCode: 'https://github.com/devemit/password-generator',
+      'https://github.com/devemit/cinemania-movies/assets/107273888/0a1212f5-ba2f-4446-9406-c90bd6dffbe8',
+    tech: ['AstroJS', 'CSS'],
+    liveSite: 'https://astro-layout.netlify.app/',
+    seeCode: 'https://github.com/devemit/astrojs',
     description:
-      'Create a secure and customizable password generator with this TypeScript and React application. Generate strong passwords with various options like length, special characters, and numbers. Easily copy the generated passwords to your clipboard for enhanced security and convenience.',
+      'Astro is a revolutionary static site generator that simplifies web development by combining the best of both static and dynamic site approaches. In a nutshell, Astro allows developers to build websites with dynamic-like functionality while retaining the benefits of static site performance.',
+  },
+  {
+    title: 'Landing Page',
+    imgUrl:
+      'https://github.com/devemit/cinemania-movies/assets/107273888/1c92ebbf-6c58-449a-b66e-f72807a6aae7',
+    tech: ['NextJS-14', 'CSS', 'Framer Motion'],
+    liveSite: 'https://managepro.vercel.app/',
+    seeCode: 'https://github.com/devemit/next14-landing-page-manage',
+    description:
+      'Manage is a sleek and responsive landing page built with Next.js and CSS, offering a user-friendly interface for efficient project management. This modern web application ensures seamless navigation on various devices, providing a seamless experience for users.',
   },
 ];
