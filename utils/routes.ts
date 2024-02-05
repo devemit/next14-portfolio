83bbacaba0491f797c92823da081591775cc9230
Mitko <mitkoiliev505@gmail.comm>
2024-02-05 13:46:33 +0100

    route delete

--- utils/routes.ts
+++ utils/routes.ts
@@ -3,5 +3,4 @@ export const pages = [
   { label: 'about', href: '/about' },
   { label: 'projects', href: '/projects' },
   { label: 'techstack', href: '/techstack' },
-  { label: 'blog', href: '/blog' },
 ];
