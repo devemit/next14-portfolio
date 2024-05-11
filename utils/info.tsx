ce5947d4649124e90ea55f261ba3b68e1b3e2e34
devemit <mitkoiliev505@gmail.com>
2024-05-11 09:48:12 +0200

    home page avatar change

--- utils/info.tsx
+++ utils/info.tsx
@@ -1,15 +1,15 @@
-import portrait from '../public/avatar.jpg';
+import mit from '../public/mit.jpg';
 
 export const name = 'Mitko Iliev.';
-export const me = portrait;
+export const me = mit;
 export const about = () => {
-   return (
-      <>
-         A Frontend focused Web Developer building the Frontend of Websites and Web Applications
-         that leads to the success of the overall product.
-      </>
-   );
+    return (
+        <>
+            A Frontend focused Web Developer building the Frontend of Websites and Web Applications
+            that leads to the success of the overall product.
+        </>
+    );
 };
 export const info = () => {
-   return <>Hey, I am Mitko, front-end developer,optimist and digital architect.</>;
+    return <>Hey, I am Mitko, front-end developer,optimist and digital architect.</>;
 };
