cf69220da988b5e76a9c80055fc16752903eb2d4
devemit <mitkoiliev505@gmail.com>
2024-07-09 19:40:45 +0200

    /

--- utils/routes.ts
+++ utils/routes.ts
@@ -2,5 +2,4 @@ export const pages = [
     { label: 'about', href: '/about' },
     { label: 'projects', href: '/projects' },
     { label: 'techstack', href: '/techstack' },
-    { label: 'blog', href: '/blog' },
 ];
