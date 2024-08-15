83922469a5469791abf11e62a6683bfbe13dae42
devemit <mitkoiliev505@gmail.com>
2024-08-15 10:31:55 +0200

    blog section added

--- app/(routes)/blog/page.tsx
+++ app/(routes)/blog/page.tsx
@@ -9,25 +9,25 @@ export default function Blogs() {
             <main className='flex flex-col gap-2'>
                 {blogs.map((blog, index) => {
                     return (
-                        <Link href={`/blog/${blog.slug}`}>
-                            <div className='flex items-center justify-between gap-4 border p-2 rounded-md cursor-pointer transition ease-in-out hover:scale-105'>
-                                <div>
-                                    <ul key={index}>
+                        <ul key={index}>
+                            <Link href={`/blog/${blog.slug}`}>
+                                <div className='flex items-center justify-between gap-4 border p-2 rounded-md cursor-pointer transition ease-in-out hover:scale-105'>
+                                    <div>
                                         <div className='text-[#D4D4D4] text-xs mb-1'>
                                             {blog.date}
                                         </div>
                                         <div className='text-[#D4D4D4]'>{blog.name}</div>
                                         <span className='text-xs text-[#D4D4D4]'>{blog.tools}</span>
-                                    </ul>
-                                </div>
+                                    </div>
 
-                                <div>
-                                    <span className='text-[#D4D4D4]'>
-                                        <FiArrowUpRight />
-                                    </span>
+                                    <div>
+                                        <span className='text-[#D4D4D4]'>
+                                            <FiArrowUpRight />
+                                        </span>
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
+                            </Link>
+                        </ul>
                     );
                 })}
             </main>
