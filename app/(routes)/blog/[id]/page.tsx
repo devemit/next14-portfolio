43133ec407e70be803959f3f6d7e8b6467b851b4
devemit <mitkoiliev505@gmail.com>
2024-08-15 15:06:49 +0200

    blog desc change

--- app/(routes)/blog/[id]/page.tsx
+++ app/(routes)/blog/[id]/page.tsx
@@ -6,11 +6,19 @@ export interface Blog {
     slug: string;
     name: string;
     description: string;
+    skills: string;
 }
 
 export default function Page() {
     const params = useParams();
     const blog: Blog | undefined = blogs.find((blog) => blog.slug === params.id);
 
-    return <div className='text-[#D4D4D4] max-w-md text-sm py-2'>{blog?.description}</div>;
+    return (
+        <div className='text-[#D4D4D4] max-w-md text-sm py-2'>
+            <div className='leading-6'>{blog?.description}</div>
+            <br />
+            <p className='text-base my-1'>Tools used:</p>
+            <div className='leading-6'>{blog?.skills}</div>
+        </div>
+    );
 }
--- utils/blogs.ts
+++ utils/blogs.ts
@@ -5,6 +5,7 @@ export default [
         name: 'Enhancing ECG Viewer',
         tools: 'Blazor .NET',
         description:
-            'In my latest project, I focused on improving the ECG viewer application by adding advanced visualization and print functionalities. I’ve implemented a feature to display ECG data across four different charts, each representing distinct time segments. For printing, I created a dynamic system that accurately numbers pages (e.g., "1/2", "2/2") and positions them at the top right corner.',
+            'In my recent development efforts, I enhanced an ECG viewer application with advanced features for improved data visualization and user experience. I implemented functionality to display ECG data across four distinct charts, each showing different time segments. This enhancement enables users to analyze ECG readings from various perspectives, providing more detailed insights.The project involved integrating these technologies to create a seamless experience for visualizing and interacting with ECG data, with further enhancements planned to refine the user interface and functionality.',
+        skills: 'Blazor: Utilized for developing interactive web components and managing state within the application, JavaScript: Employed to create dynamic page functionalities and handle complex interactions with the DOM,CSS: Applied for styling and ensuring consistent visual presentation of the ECG data and other UI elements.',
     },
 ];
