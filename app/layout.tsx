8c7fbb33038e6c7910c9bbe3a917f8d9e631d997
devemit <mitkoiliev505@gmail.com>
2025-02-14 16:20:10 +0100

    resened integrate

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,26 +2,30 @@ import './globals.css';
 import { Metadata } from 'next';
 import Navbar from '@/components/navbar';
 import localFont from 'next/font/local';
+import { ContactForm } from '@/components/contact-form';
 
 const graphik = localFont({
-    src: '../public/fonts/GraphikRegular.otf',
-    weight: '400',
-    variable: '--font-graphik',
-    display: 'swap',
+   src: '../public/fonts/GraphikRegular.otf',
+   weight: '400',
+   variable: '--font-graphik',
+   display: 'swap',
 });
 
 export const metadata: Metadata = {
-    title: 'Mitko',
-    description: 'Portfolio Application',
+   title: 'Mitko',
+   description: 'Portfolio Application',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang='en' className={graphik.className}>
-            <body className='bg-[#111010] px-4 py-12 flex flex-col md:flex-row max-w-4xl mx-4 md:mt-20 lg:mt-32 lg:mx-auto gap-12'>
-                <Navbar />
-                {children}
-            </body>
-        </html>
-    );
+   return (
+      <html lang='en' className={graphik.className}>
+         <body className='bg-[#111010] px-4 py-12 flex flex-col md:flex-row max-w-4xl mx-4 md:mt-20 lg:mt-32 lg:mx-auto gap-12'>
+            <Navbar />
+            {children}
+            <div className='absolute bottom-0 right-0'>
+               <ContactForm />
+            </div>
+         </body>
+      </html>
+   );
 }
