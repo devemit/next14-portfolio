4b33296c2d5669be2854a3ccc0967a189f4f4dc7
Mitko <mitkoiliev505@gmail.com>
2025-09-08 12:25:08 +0200

    fix: line height in blogs

--- app/(routes)/blog/[id]/page.tsx
+++ app/(routes)/blog/[id]/page.tsx
@@ -16,10 +16,10 @@ export default function Page() {
    return (
       <div className='text-[#D4D4D4] max-w-md mx-auto'>
          <h1 className='text-2xl font-bold mb-4'>{blog?.name}</h1>
-         <div className='leading-6 mb-4'>{blog?.description}</div>
+         <div className='leading-relaxed mb-4'>{blog?.description}</div>
          <br />
          <p className='text-base my-1 font-semibold'>Tools used:</p>
-         <div className='leading-6'>{blog?.skills}</div>
+         <div className='leading-relaxed'>{blog?.skills}</div>
       </div>
    );
 }
