0e8669835f23846afe97670ffacb468ca7f249d5
Mitko <mitkoiliev505@gmail.com>
2025-11-05 00:01:03 +0100

    project order update

--- utils/projects.ts
+++ utils/projects.ts
@@ -1,13 +1,13 @@
 export default [
   {
-    title: 'Easy Travel',
-    status: 'wip',
-    imgUrl: 'https://github.com/user-attachments/assets/eec04cfa-c5b0-4457-8247-4b6ae2c61729',
-    tech: ['Typescript', 'NextJS', 'Tailwind CSS', 'PostgreSQL', 'Prisma'],
-    liveSite: 'https://travelsmart-devemits-projects.vercel.app/',
-    seeCode: 'https://github.com/devemit/smart-travel',
+    title: 'Aduvi – Business Management Platform',
+    status: '',
+    imgUrl: 'https://i.postimg.cc/jjcfD3gv/screencapture-aduvi-2025-11-01-18-09-31-2.png',
+    tech: ['Typescript', 'ReactJS', 'SASS', 'Redux', 'Ant Design'],
+    liveSite: 'https://aduvi.com/',
+    seeCode: 'https://aduvi.com/',
     description:
-      'Easy Travel is an innovative travel web application currently in beta phase, allowing users to explore destinations, check the weather, and plan their trips with the help of AI. Users can browse featured locations, access essential travel information, and receive personalized recommendations to enhance their travel experience. With a user-friendly interface, Easy Travel makes trip planning simple and enjoyable.',
+      'Current Work: I joined the frontend development team at Technoperia in September 2024 to work on Aduvi, a comprehensive enterprise business management platform serving 2000+ clients across multiple industries. Working collaboratively with a talented team of developers, I contribute to building this all-in-one solution for managing leads, bookings, events, custom forms, and client communication through a dedicated portal. My focus is on developing responsive, user-friendly interfaces and ensuring a smooth user experience across the platform, while implementing advanced features like real-time data synchronization, multi-view management systems, and comprehensive business automation tools.',
   },
   {
     title: 'View ECG',
@@ -21,14 +21,14 @@ export default [
       "Current Work: I joined the development team in August 2025 to work on this innovative AI-powered healthcare platform that connects doctors and patients. Working alongside a multidisciplinary team of healthcare professionals and developers, I contribute to building and enhancing the cloud-based ECG monitoring system. Our cloud system seamlessly integrates with measurements from mobile devices and enables comprehensive data analysis. The platform features special beat detection and classification algorithms designed by a team of doctors and computer scientists. Among the capabilities we've developed are:\nBeat classification (Normal, PVC, VEB, PAC, SVEB)\nRun-to-run identification (ventricular and supraventricular couplets, short and long runs)\nEpisode identification (atrial fibrillation, ventricular tachycardia, bigeminy, trigeminy)",
   },
   {
-    title: 'Aduvi – Business Management Platform',
-    status: '',
-    imgUrl: 'https://i.postimg.cc/jjcfD3gv/screencapture-aduvi-2025-11-01-18-09-31-2.png',
-    tech: ['Typescript', 'ReactJS', 'SASS', 'Redux', 'Ant Design'],
-    liveSite: 'https://aduvi.com/',
-    seeCode: 'https://aduvi.com/',
+    title: 'Easy Travel',
+    status: 'wip',
+    imgUrl: 'https://github.com/user-attachments/assets/eec04cfa-c5b0-4457-8247-4b6ae2c61729',
+    tech: ['Typescript', 'NextJS', 'Tailwind CSS', 'PostgreSQL', 'Prisma'],
+    liveSite: 'https://travelsmart-devemits-projects.vercel.app/',
+    seeCode: 'https://github.com/devemit/smart-travel',
     description:
-      'Current Work: I joined the frontend development team at Technoperia in September 2024 to work on Aduvi, a comprehensive enterprise business management platform serving 2000+ clients across multiple industries. Working collaboratively with a talented team of developers, I contribute to building this all-in-one solution for managing leads, bookings, events, custom forms, and client communication through a dedicated portal. My focus is on developing responsive, user-friendly interfaces and ensuring a smooth user experience across the platform, while implementing advanced features like real-time data synchronization, multi-view management systems, and comprehensive business automation tools.',
+      'Easy Travel is an innovative travel web application currently in beta phase, allowing users to explore destinations, check the weather, and plan their trips with the help of AI. Users can browse featured locations, access essential travel information, and receive personalized recommendations to enhance their travel experience. With a user-friendly interface, Easy Travel makes trip planning simple and enjoyable.',
   },
   {
     title: 'Reform Beach&Bar',
