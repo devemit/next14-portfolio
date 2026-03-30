f70d1ea1356e5da87d287c69c5c7e59414d5c6c8
Mitko <mitkoiliev505@gmail.com>
2026-03-30 14:19:15 +0200

    update: refine about section for clarity and conciseness

--- utils/info.tsx
+++ utils/info.tsx
@@ -6,13 +6,11 @@ export const about = () => {
       Software Developer, Designer and Creator.
       <br />
       <br />
-      I&apos;m a self-taught Software Developer and made the transition from my previous career to
-      tech in 2023.
+      I&apos;m a self-taught Software Developer and made the transition from my previous career to tech in 2023.
       <br />
       <br />
-      I&apos;ve had the pleasure of working alongside some incredible engineers, designers. I&apos;m
-      passionate about code standards, ECMAScript, strongly typed interfaces, and reactive
-      Javascript frameworks (React is my jam).
+      I&apos;ve had the pleasure of working alongside some incredible engineers, designers. I&apos;m passionate about code standards,
+      ECMAScript, strongly typed interfaces, and reactive Javascript frameworks.
     </>
   )
 }
